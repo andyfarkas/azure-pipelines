576d56d160249faa7b15a1f849e5c62b6718ac63
Andrej Farkas <andrej.farkas@sap.com>
2023-03-10 13:41:09 +0100

    Added testing annoattions

--- src/test/java/org/andyfarkas/SimpleClassTests.java
+++ src/test/java/org/andyfarkas/SimpleClassTests.java
@@ -1,5 +1,6 @@
 package org.andyfarkas;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -8,11 +9,13 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class SimpleClassTests {
 
     @Test
+    @DisplayName("Test that the method returns true")
     public void success() {
         assertTrue(new SimpleClass().method());
     }
 
     @Test
+    @DisplayName("Test that the method returns true")
     public void fail() {
         assertFalse(new SimpleClass().method());
     }
